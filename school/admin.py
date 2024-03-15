26f9eed5c35b3b47b45844552f1b6c04a6da9b85
Aleksandra Ivanova <sashaivanova90@mail.ru>
2024-03-15 17:12:05 +0300

    Initial commit

---
+++ school/admin.py
@@ -0,0 +1,21 @@
+from django.contrib import admin
+
+from .models import Student, Teacher
+
+class TeachersInline(admin.TabularInline):
+    model = Student.teachers.through
+    extra = 1
+
+
+@admin.register(Student)
+class StudentAdmin(admin.ModelAdmin):
+    inlines = [
+        TeachersInline,
+    ]
+
+
+@admin.register(Teacher)
+class TeacherAdmin(admin.ModelAdmin):
+    inlines = [
+        TeachersInline,
+    ]
