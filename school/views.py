88df08c83d7f26bf1439e9b452cae02260b258e0
Aleksandra Ivanova <sashaivanova90@mail.ru>
2024-03-18 14:21:03 +0300

    Setting Django Debug Toolbar

--- school/views.py
+++ school/views.py
@@ -6,6 +6,6 @@ from .models import Student
 
 def students_list(request):
     template = 'school/students_list.html'
-    object_list = Student.objects.order_by('group')
+    object_list = Student.objects.prefetch_related("teachers").order_by('group')
     context = {'object_list': object_list}
     return render(request, template, context)
--- website/settings.py
+++ website/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'debug_toolbar',
+
     'school',
 ]
 
@@ -49,6 +51,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
 ]
 
 ROOT_URLCONF = 'website.urls'
@@ -135,3 +138,6 @@ try:
 except ImportError:
     pass
 
+INTERNAL_IPS = [
+    '127.0.0.1',
+]
